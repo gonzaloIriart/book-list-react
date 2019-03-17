c1cf80ef3312f346020f07a9f61f4e331974ad7a
GonzaloIriart <gonzaloiriart@gmail.com>
2019-03-17 00:09:08 -0300

    added data

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 export default class Book extends Component {
   render() {
-     const {name,author,description,links} = this.props.info;
+     const {name,img,author,description,links} = this.props.info;
     return (
       <div>
+        <img src = {img} alt= "book cover" />
         <h3>{name}</h3>
         <h4>{author}</h4>
         <p>{description}</p>
--- src/components/BookList/data.js
+++ src/components/BookList/data.js
@@ -1,24 +1,27 @@
 const books = [
     {
         id:1,
-        img:'',
-        name: "Mistborne",
+        img:"",
+        name: "Mistborn",
         author: "Brandon Sanderson",
-        description: "first book of the Mistborne trilogy"
+        description: "first book of the Mistborne trilogy",
+        links:""
     },
     {
         id:2,
-        img:'',
+        img:"",
         name: "The well of ascension",
         author: "Brandon Sanderson",
-        description: "second book of the Mistborne trilogy"
+        description: "second book of the Mistborne trilogy",
+        links:""
     },
     {
         id:3,
-        img:'',
+        img:"",
         name: "The hero of ages",
         author: "Brandon Sanderson",
-        description: "third book of the Mistborne trilogy"
+        description: "third book of the Mistborne trilogy",
+        links:""
     }
 ]
 
