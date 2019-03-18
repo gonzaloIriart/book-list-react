3e961c3cff07b0a84ae67128d2ee0b359619d0de
GonzaloIriart <gonzaloiriart@gmail.com>
2019-03-18 12:16:01 -0300

    added style

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './Book.css';
 
 export default class Book extends Component {
   render() {
      const {name,img,author,description,links} = this.props.info;
     return (
-      <div>
+      <div className= "book">
         <img src = {img} alt= "book cover" />
         <h3>{name}</h3>
         <h4>{author}</h4>
--- src/components/BookList/data.js
+++ src/components/BookList/data.js
@@ -1,7 +1,7 @@
 const books = [
     {
         id:1,
-        img:"",
+        img:"https://images-na.ssl-images-amazon.com/images/I/91VZMbqbuZL.jpg",
         name: "Mistborn",
         author: "Brandon Sanderson",
         description: "first book of the Mistborne trilogy",
@@ -9,7 +9,7 @@ const books = [
     },
     {
         id:2,
-        img:"",
+        img:"https://images-na.ssl-images-amazon.com/images/I/81v-%2Bsxyw9L.jpg",
         name: "The well of ascension",
         author: "Brandon Sanderson",
         description: "second book of the Mistborne trilogy",
@@ -17,7 +17,7 @@ const books = [
     },
     {
         id:3,
-        img:"",
+        img:"https://images-na.ssl-images-amazon.com/images/I/81RPa1UMMwL.jpg",
         name: "The hero of ages",
         author: "Brandon Sanderson",
         description: "third book of the Mistborne trilogy",
